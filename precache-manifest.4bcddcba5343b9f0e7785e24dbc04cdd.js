2c811a1987d556fe0dc1ff17d50b99dd16eb77fe
h2ash <hun22ash@gmail.com>
2019-08-13 21:54:16 +0300

    Updates

---
+++ precache-manifest.4bcddcba5343b9f0e7785e24dbc04cdd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23ed52ffc0e4715a89ea1ef83ed0c7c6",
+    "url": "/first-redux/index.html"
+  },
+  {
+    "revision": "0fc069199afc26860008",
+    "url": "/first-redux/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "20101e98816bdd408345",
+    "url": "/first-redux/static/js/2.3a05a169.chunk.js"
+  },
+  {
+    "revision": "0fc069199afc26860008",
+    "url": "/first-redux/static/js/main.c2fe2e00.chunk.js"
+  },
+  {
+    "revision": "43791bfa2e9e3fffca17",
+    "url": "/first-redux/static/js/runtime~main.5a84993c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/first-redux/static/media/logo.5d5d9eef.svg"
+  }
+]);
